ec854bd642910b80672c4b3759f9ee8c995ee604
Beto1807 <albertorodriguezcazares@gmail.com>
2023-05-27 15:13:54 -0600

    Eliminados links innecesarios

--- app/page.tsx
+++ app/page.tsx
@@ -28,24 +28,6 @@ export default function Home() {
       </Suspense>
 
       <div className="flex justify-center space-x-5 pt-10 mt-10 border-t border-gray-300 w-full max-w-xl text-gray-600">
-        <Link
-          href="https://postgres-prisma.vercel.app/"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Prisma
-        </Link>
-        <Link
-          href="https://postgres-kysely.vercel.app/"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Kysely
-        </Link>
-        <Link
-          href="https://postgres-drizzle.vercel.app/"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Drizzle
-        </Link>
       </div>
 
       <div className="sm:absolute sm:bottom-0 w-full px-20 py-10 flex justify-between">
@@ -53,8 +35,8 @@ export default function Home() {
           <Image
             src="/samos.svg"
             alt="SAMOS Logo"
-            width={100}
-            height={24}
+            width={50}
+            height={50}
             priority
           />
         </Link>
