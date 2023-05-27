af6704ccdd5accd7955e0911a0611c8348541fb1
Beto1807 <albertorodriguezcazares@gmail.com>
2023-05-27 17:14:52 -0600

    caca

--- app/page.tsx
+++ app/page.tsx
@@ -16,17 +16,13 @@ export default function Home() {
       <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
         Palabra del día de: 
       </h1> 
-      <div className="flex items-center">
+      <div className="flex items-center" style="margin: 50px;">
         <Image 
             src="/logo.svg"
             alt="MIKU Logo"
             width={300}
             //height={15}
           />
-      </div>
-
-      <div className="flex items-center">
-      <p>  </p>
       </div>
       
       <Suspense fallback={<TablePlaceholder />}>
