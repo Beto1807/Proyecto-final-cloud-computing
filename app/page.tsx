1994f78031b8e9226c093f7218fe06d55982ad68
Beto1807 <albertorodriguezcazares@gmail.com>
2023-05-27 13:50:34 -0600

    Primera personalizacion

--- app/page.tsx
+++ app/page.tsx
@@ -16,32 +16,16 @@ export default function Home() {
         href="https://vercel.com/templates/next.js/postgres-starter"
         className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
       >
-        <p>Deploy your own to Vercel</p>
+        <p>Iniciar sesión</p>
         <ExpandingArrow />
       </Link>
       <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
-        Postgres on Vercel
+        Palabra del día de Hatsune Miku
       </h1>
       <Suspense fallback={<TablePlaceholder />}>
         {/* @ts-expect-error Async Server Component */}
         <Table />
       </Suspense>
-      <p className="font-light text-gray-600 w-full max-w-lg text-center mt-6">
-        <Link
-          href="https://vercel.com/postgres"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Vercel Postgres
-        </Link>{' '}
-        demo. <br /> Built with{' '}
-        <Link
-          href="https://nextjs.org/docs"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Next.js App Router
-        </Link>
-        .
-      </p>
 
       <div className="flex justify-center space-x-5 pt-10 mt-10 border-t border-gray-300 w-full max-w-xl text-gray-600">
         <Link
@@ -65,17 +49,17 @@ export default function Home() {
       </div>
 
       <div className="sm:absolute sm:bottom-0 w-full px-20 py-10 flex justify-between">
-        <Link href="https://vercel.com">
+        <Link href="">
           <Image
-            src="/vercel.svg"
-            alt="Vercel Logo"
+            src="/samos.svg"
+            alt="SAMOS Logo"
             width={100}
             height={24}
             priority
           />
         </Link>
         <Link
-          href="https://github.com/vercel/examples/tree/main/storage/postgres-starter"
+          href="https://github.com/Beto1807/Proyecto-final-cloud-computing"
           className="flex items-center space-x-2"
         >
           <Image
@@ -85,7 +69,7 @@ export default function Home() {
             height={24}
             priority
           />
-          <p className="font-light">Source</p>
+          <p className="font-light">Código</p>
         </Link>
       </div>
     </main>
