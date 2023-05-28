6ec2805ffc57ac94a871912469db0ff237d201ec
Beto1807 <albertorodriguezcazares@gmail.com>
2023-05-28 15:46:35 -0600

    hola

--- components/login.tsx
+++ components/login.tsx
@@ -10,7 +10,7 @@ export default async function Login() {
     return (
       <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
         <div className="flex justify-center space-x-5 pt-10 mt-10 border-t border-gray-300 w-full max-w-xl text-gray-600">
-          <form className="w-full max-w-lg" onSubmit={handleSubmit}>
+          <form className="w-full max-w-lg" onSubmit={handleSubmit()}>
             <label htmlFor="#email" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Correo</label>
             <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               placeholder="Ingrese correo"
@@ -82,21 +82,26 @@ export default async function Login() {
 }
 
 async function handleSubmit(e) {
-  e.preventDefault();
-  var formData = new FormData(e.target);
+  //e.preventDefault();
+  //var formData = new FormData(e.target);
   let login
 
   try {
-    var email = formData.get("email");
-    var pass = formData.get("password");
-    login = await sql`SELECT * FROM users where email = '${email}' and password = '${pass}'`
-  
-    let { rows: result } = login
-  
-    if (result.length >= 1) {
-      var nam = result[0].name
-      sessionStorage.setItem("name", nam);
-      Refresh()
+    //var email = formData.get("email");
+    //var pass = formData.get("password");
+
+    if(email && pass){
+      var email = document.querySelector('#email').value;
+      var pass = document.querySelector('#password').value;
+      login = await sql`SELECT * FROM users where email = '${email}' and password = '${pass}'`
+    
+      let { rows: result } = login
+    
+      if (result.length >= 1) {
+        var nam = result[0].name
+        sessionStorage.setItem("name", nam);
+        Refresh()
+      }
     }
 
   } catch (e: any) {
